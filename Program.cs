2e53288df7a5a5155bcd8e2def5bcaebc7276a25
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2022-12-19 01:39:05 +0300

    task37 is done

--- Program.cs
+++ Program.cs
@@ -1,29 +1,34 @@
-﻿// Задача 38: Задайте массив вещественных чисел.
-// Найдите разницу между максимальным и минимальным элементов массива.
+﻿// Задача 37 со звездочкой, дополнительная: Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
 
-// [3 7 22 2 78] -> 76
-double [] array = new double [4];
-void FillArray (double [] array1)
+// Сломал себе мозг, пока сделал. Наверняка можно проще, но как додумал...))
+
+int [] array = new int [7];
+void FillArray (int [] array1)
 {
     for (int i = 0; i < array1.Length; i++)
     {
-        array[i] = new Random().NextDouble();
-        array[i] = Math.Round (array[i], 3);
+        array[i] = new Random().Next(1,10);
     }
 }
 FillArray(array);
 System.Console.WriteLine($"Array: [{String.Join(", ", array)}]");
-System.Console.WriteLine();
-void FindMaxAndMin (double [] array2)
+void CouplesProduct (int [] array2)
 {
-    double max = array2[0];
-    double min = array2[0];
-    for (int i = 1; i < array2.Length; i++)
+    int size = array2.Length;
+    int size2 = size/2;
+    if (size%2 == 1) size2=(size/2) +1;
+    int j = size-1;
+    int [] array3 = new int [size2];
+    for (int i = 0; i < size/2; i++)
     {
-        if (array[i] > max) max = array [i];
-        if (array[i] < min) min = array [i];
+        int product = array2[i] * array2[j];
+        array3[i] = product;
+        j--;
     }
-    double dif = max - min;
-    System.Console.WriteLine($"Разница между макс.эл. {max} и мин.эл. {min} = {dif}");
+    if (size%2 == 1) array3[j] = array[j];
+    System.Console.WriteLine($"Массив из произведений пар массива : [{String.Join(", ", array3)}]");
 }
-FindMaxAndMin(array);
+CouplesProduct(array);
