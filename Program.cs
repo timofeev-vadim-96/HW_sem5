b0455f7dbad39ffc889be4d85fe7bd99b737ad1e
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2022-12-18 23:55:06 +0300

    task34 is ready

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+//  Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+int [] array = new int [9];
+void FillArray (int [] array1)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+        array[i] = new Random().Next(100,1000);
+    }
+}
+FillArray(array);
+System.Console.WriteLine($"Array: [{String.Join(", ", array)}]");
+
+void CalculateEvenNumbers (int [] array2)
+{
+    int count = 0;
+    foreach (var item in array2)
+    {
+        if (item%2 == 0) count++;
+    }
+    System.Console.WriteLine($"Количество четных чисел в массиве = {count}");
+}
+CalculateEvenNumbers(array);
